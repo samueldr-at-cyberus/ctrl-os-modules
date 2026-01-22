5b04a6af6729473f03404266445b9622415e9f85
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-22 12:51:20 -0500

    modules/platform: Make generic and self-describing via structure

--- modules/platform.nix
+++ modules/platform.nix
@@ -1,16 +1,20 @@
 { lib, ... }:
+
+let
+  inherit
+    (import ../lib { inherit lib; })
+    getVendorsModules
+  ;
+  deviceModules = getVendorsModules ./platforms/devices;
+  devices = builtins.attrNames deviceModules;
+  deviceDirs = builtins.attrValues deviceModules;
+in
 {
   options.ctrl-os.platform = lib.mkOption {
-    type =
-      with lib.types;
-      nullOr (enum [
-        "nvidia-jetson-orin-nano-super"
-      ]);
+    type = with lib.types; nullOr (enum devices);
     description = "The platform, we are running on.";
     default = null;
   };
 
-  imports = [
-    ./platforms/devices/nvidia/jetson-orin-nano-super/default.nix
-  ];
+  imports = deviceDirs;
 }
