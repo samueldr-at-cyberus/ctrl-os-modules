3245d525c3e3aeeb22857275538ae22560a2b010
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-22 19:58:51 -0500

    devices/nvidia/jetson-orin-nano-super: Remove gating from module

--- modules/platforms/devices/nvidia/jetson-orin-nano-super/default.nix
+++ modules/platforms/devices/nvidia/jetson-orin-nano-super/default.nix
@@ -1,19 +1,14 @@
-{ config, lib, ... }:
-let
-  platform = config.ctrl-os.platform;
-in
+{ lib, ... }:
+
 {
-  config = lib.mkIf (platform == "nvidia-jetson-orin-nano-super") {
+  nixpkgs.hostPlatform = "aarch64-linux";
 
-    nixpkgs.hostPlatform = "aarch64-linux";
-
-    boot.initrd.availableKernelModules = [
-      # Enable PCIe support at boot time
-      "phy_tegra194_p2u"
-      "pcie_tegra194"
-      # Enable USB support for USB Boot
-      "xhci-tegra"
-      "phy-tegra-xusb"
-    ];
-  };
+  boot.initrd.availableKernelModules = [
+    # Enable PCIe support at boot time
+    "phy_tegra194_p2u"
+    "pcie_tegra194"
+    # Enable USB support for USB Boot
+    "xhci-tegra"
+    "phy-tegra-xusb"
+  ];
 }
