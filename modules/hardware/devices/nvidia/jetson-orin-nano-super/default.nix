23199ff72f36bf20400952dad4611685bcea17ac
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-01-29 16:33:30 -0500

    jetson-orin-nano-super: Prepare for adding `options`
    This helps make the follow-up commit easier to review.

--- modules/hardware/devices/nvidia/jetson-orin-nano-super/default.nix
+++ modules/hardware/devices/nvidia/jetson-orin-nano-super/default.nix
@@ -1,12 +1,14 @@
 {
-  nixpkgs.hostPlatform = "aarch64-linux";
+  config = {
+    nixpkgs.hostPlatform = "aarch64-linux";
 
-  boot.initrd.availableKernelModules = [
-    # Enable PCIe support at boot time
-    "phy_tegra194_p2u"
-    "pcie_tegra194"
-    # Enable USB support for USB Boot
-    "xhci-tegra"
-    "phy-tegra-xusb"
-  ];
+    boot.initrd.availableKernelModules = [
+      # Enable PCIe support at boot time
+      "phy_tegra194_p2u"
+      "pcie_tegra194"
+      # Enable USB support for USB Boot
+      "xhci-tegra"
+      "phy-tegra-xusb"
+    ];
+  };
 }
